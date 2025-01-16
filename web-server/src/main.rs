a6bfae8047983ed17b24eb944f526a6c527beb1b
尤雨东 <root@ltpp.vip>
2025-01-16 08:53:11 +0800

    feat:dependencies

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,22 +1,26 @@
 use hyperlane::*;
 
-fn run_server() {
+async fn run_server() {
     let mut server: Server = Server::new();
     server.host("0.0.0.0");
     server.port(60000);
     server.log_dir("./logs");
     server.log_size(1_024_000);
-    server.router("/", |data| {
-        let stream: Arc<TcpStream> = data.get_stream().clone().unwrap();
-        data.get_mut_response()
-            .set_body("hello".into())
-            .set_header("server", "test")
-            .send(&stream)
-            .unwrap();
-    });
+    server.thread_pool_size(8);
+    server
+        .async_router("/", |arc_lock_data: ArcRwLockControllerData| async move {
+            let mut data: RwLockWriteControllerData = arc_lock_data.write().unwrap();
+            let stream: Arc<TcpStream> = data.get_stream().clone().unwrap();
+            data.get_mut_response()
+                .set_body("hello".into())
+                .send(&stream)
+                .unwrap();
+        })
+        .await;
     server.listen();
 }
 
-fn main() {
-    run_server();
+#[tokio::main]
+async fn main() {
+    run_server().await;
 }
