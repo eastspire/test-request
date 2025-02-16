843ebde1be46341d542a81ef20e7205400d318f1
尤雨东 <root@ltpp.vip>
2025-02-16 11:49:49 +0800

    feat: dependencies

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,23 +1,20 @@
 use hyperlane::*;
 
-async fn run_server() {
+fn test_sync_middleware(arc_lock_controller_data: ArcRwLockControllerData) {
+    let _ = send_response(&arc_lock_controller_data, 200, "hello");
+}
+
+fn run_server() {
     let mut server: Server = Server::new();
     server.host("0.0.0.0");
     server.port(60000);
     server.log_dir("./logs");
-    server.log_size(1_024_000);
-    server
-        .async_router(
-            "/",
-            |arc_lock_controller_data: ArcRwLockControllerData| async move {
-                let _ = send_response(&arc_lock_controller_data, 200, "hello");
-            },
-        )
-        .await;
+    server.log_interval_millis(1_000_000_000);
+    server.middleware(test_sync_middleware);
     server.listen();
 }
 
 #[tokio::main]
 async fn main() {
-    run_server().await;
+    run_server();
 }
