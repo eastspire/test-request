18b7b9b749fa1871779813a98d4dd6cfbaff1eca
尤雨东 <root@ltpp.vip>
2025-02-19 12:44:00 +0800

    feat: dependencies

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,28 +1,29 @@
 use hyperlane::*;
 
-fn test_sync_middleware(arc_lock_controller_data: ArcRwLockControllerData) {
-    let controller_data: ControllerData = get_controller_data(&arc_lock_controller_data);
+async fn middleware(arc_lock_controller_data: ArcRwLockControllerData) {
+    let controller_data: ControllerData = get_controller_data(&arc_lock_controller_data).await;
     let mut response: Response = controller_data.get_response().clone();
     let body: &str = "hello";
-    let stream: ArcTcpStream = controller_data.get_stream().clone().unwrap();
+    let stream: ArcRwLockStream = controller_data.get_stream().clone().unwrap();
     let _: ResponseResult = response
         .set_body(body)
         .set_status_code(200)
         .set_header(CONNECTION, CONNECTION_KEEP_ALIVE)
-        .send(&stream);
+        .send(&stream)
+        .await;
 }
 
-fn run_server() {
+async fn run_server() {
     let mut server: Server = Server::new();
-    server.host("0.0.0.0");
-    server.port(60000);
-    server.log_dir("./logs");
-    server.log_interval_millis(1_000_000_000);
-    server.middleware(test_sync_middleware);
-    server.listen();
+    server.host("0.0.0.0").await;
+    server.port(60000).await;
+    server.log_dir("./logs").await;
+    server.log_interval_millis(1_000_000_000).await;
+    server.middleware(middleware).await;
+    server.listen().await;
 }
 
 #[tokio::main]
 async fn main() {
-    run_server();
+    run_server().await;
 }
