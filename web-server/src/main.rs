8d2b00ce9db3f182eb04c9da6d2b4af7f2dd292c
尤雨东 <root@ltpp.vip>
2025-03-23 15:47:42 +0800

    feat: hyperlane

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -9,11 +9,13 @@ async fn request_middleware(controller_data: ControllerData) {
 }
 
 async fn run_server() {
-    let mut server: Server = Server::new();
+    let server: Server = Server::new();
     server.host("0.0.0.0").await;
     server.port(60000).await;
     server.log_dir("./logs").await;
     server.log_interval_millis(1_000_000_000).await;
+    server.http_line_buffer_size(512).await;
+    server.websocket_buffer_size(512).await;
     server.request_middleware(request_middleware).await;
     server.listen().await;
 }
